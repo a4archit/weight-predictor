1dbc2144e60a7f8a6fabe59cd129305f86aa5f8d
Archit <archittyagi599@gmail.com>
2024-11-17 01:17:06 +0000

    updation sklarn

--- weight_predictor.py
+++ weight_predictor.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import streamlit as st
 import pickle
-
+import sklearn
 model = pickle.load(open('weight_predictor_model.pkl', 'rb'))
 
 # ------------------------------------------- GUI: starts from here ----------------------------------
